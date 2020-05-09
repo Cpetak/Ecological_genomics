99c628853fe2df741415b860bacf812ebcf7fe45
CP <csenge.petak@uvm.edu>
2020-05-09 10:28:11 -0400

    some updates

--- other/SFS_processing.R
+++ other/SFS_processing.R
@@ -1,18 +1,49 @@
-data <- read.delim("XCV.thetas", header = TRUE, sep = "\t" )
 
 
-all_pstW <- c(0)
-for (i in seq(length(data$tW))){
-  all_pstW[i] <- data$tW[i]/data$nSites[i]
-}
+SFS <- scan("HD_allsites.sfs")
+sumSFS <- sum(SFS)
+pctPoly = 100*(1-(SFS[1]/sumSFS))
+plotSFS_HD <- SFS[2:length(SFS)]
+barplot(plotSFS_HD, names.arg = seq(1,length(SFS)-1), xlab="Derived allele frequency", ylab="Number of sites", col=rgb(1, 0, 0, 0.5))
 
-all_pstP <- c(0)
-for (i in seq(length(data$tP))){
-  all_pstP[i] <- data$tP[i]/data$nSites[i]
-}
+SFS <- scan("CW_allsites.sfs")
+sumSFS <- sum(SFS)
+pctPoly = 100*(1-(SFS[1]/sumSFS))
+plotSFS_CW <- SFS[2:length(SFS)]
+barplot(plotSFS_CW, col=rgb(0, 0, 1, .5),add=TRUE)
 
-hist(all_pstW, main="Histogram of per-site tW")
-hist(all_pstP, main="Histogram of per-site tP")
-hist(data$Tajima, main="Tajima's D")
+legend('topright', bty = 'n', title = 'Source climate',
+       legend = c('HotDry', 'ColdWet'), fill = c("tomato3", "dodgerblue3"))
 
+
+div <- read.table("HD_allsites.thetas.idx.pestPG")
+colnames(div)=c("window","chrome","wincenter","tW","tP","tF","tH","tL","tajD","fulif","fuliD","fayH","zengsE","numSites")
+div$tWpersite = div$tW/div$numSites
+div$tPpersite = div$tP/div$numSites
+pdf("XWS_diversity_stats2.pdf")
+par(mfrow=c(2,2))
+
+hist(div$tWpersite,col="darkorchid",xlab="Theta_W",main="")
+hist(div$tPpersite,col="darkorchid", xlab="Theta-Pi",main="")
+hist(div$tajD,col=rgb(1, 0, 0, 0.5),xlab="Tajima's D",main="")
+
+summary(div)
+
+barplot(plotSFS, main="SFS", xlab= "Derived allele frequency", ylab="Number of sites")
+dev.off()
+
+plot(div$tajD)
+
+div <- read.table("CW_allsites.thetas.idx.pestPG")
+colnames(div)=c("window","chrome","wincenter","tW","tP","tF","tH","tL","tajD","fulif","fuliD","fayH","zengsE","numSites")
+div$tWpersite = div$tW/div$numSites
+div$tPpersite = div$tP/div$numSites
+pdf("XWS_diversity_stats2.pdf")
+par(mfrow=c(2,2))
+
+hist(div$tWpersite,col="darkorchid",xlab="Theta_W",main="")
+hist(div$tPpersite,col="darkorchid", xlab="Theta-Pi",main="")
+hist(div$tajD,col=rgb(0, 0, 1, .5),xlab="Tajima's D",main="", add=TRUE)
+abline(v = 0, col="red", lwd=3, lty=2)
+summary(div)
 
